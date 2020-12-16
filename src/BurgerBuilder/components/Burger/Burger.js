27abf2c34ed7f085446c5f21e729a3358ea5ba8f
himanshu-birdeye <himanshu.yadav@birdeye.com>
2020-12-16 14:55:12 +0530

    commit testing

--- src/BurgerBuilder/components/Burger/Burger.js
+++ src/BurgerBuilder/components/Burger/Burger.js
@@ -3,22 +3,24 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    let defaultText='';
+    let defaultText = '';
     const transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
-            return [...Array(props.ingredients[igKey])].map( (_, i) => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + i} type={igKey} />;
             });
         })
-        .reduce((arr,el)=>{
+        .reduce((arr, el) => {
             return arr.concat(el);
-        },[]);
+        }, []);
 
-        if(transformedIngredients.length===0){
-            defaultText="Please add some ingredients...";}
-       
+    if (transformedIngredients.length === 0) {
+        defaultText = "Please add some ingredients...";
+    }
+
     return (
         <div className={classes.Burger}>
+            <BurgerIngredient type='bread-top'></BurgerIngredient>
             <BurgerIngredient type='bread-top'></BurgerIngredient>
             {defaultText}
             {transformedIngredients}
